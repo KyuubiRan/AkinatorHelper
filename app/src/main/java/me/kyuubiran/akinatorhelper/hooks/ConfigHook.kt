4a578db8e27d1db27577b645aee0e59d7403f983
KyuubiRan <KyuubiRan@qq.com>
2021-12-27 14:04:09 +0800

    Update version

--- app/src/main/java/me/kyuubiran/akinatorhelper/hooks/ConfigHook.kt
+++ app/src/main/java/me/kyuubiran/akinatorhelper/hooks/ConfigHook.kt
@@ -1,15 +1,19 @@
 package me.kyuubiran.akinatorhelper.hooks
 
-import com.github.kyuubiran.ezxhelper.utils.getMethodsByCondition
+import com.github.kyuubiran.ezxhelper.utils.findAllMethods
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 
 object ConfigHook : BaseHook() {
     override fun init() {
-        getMethodsByCondition("com.digidust.elokence.akinator.factories.AkConfigFactory") {
-            it.name == "isFreemium" || it.name == "isGameCountLimited" || it.name == "canShowAd"
-        }.hookBefore { it.result = false }
-        getMethodsByCondition("com.digidust.elokence.akinator.factories.AkConfigFactory") {
-            it.name == "isPaid" || it.name == "isUnlocked"
-        }.hookBefore { it.result = true }
+        findAllMethods("com.digidust.elokence.akinator.factories.AkConfigFactory") {
+            name == "isFreemium" || name == "isGameCountLimited" || name == "canShowAd"
+        }.hookBefore {
+            it.result = false
+        }
+        findAllMethods("com.digidust.elokence.akinator.factories.AkConfigFactory") {
+            name == "isPaid" || name == "isUnlocked"
+        }.hookBefore {
+            it.result = true
+        }
     }
 }
