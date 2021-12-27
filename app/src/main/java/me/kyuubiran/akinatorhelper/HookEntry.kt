0776f3dd742e7b9d5166cc7e1ed8e7e386fe2c13
KyuubiRan <KyuubiRan@qq.com>
2021-12-27 14:05:33 +0800

    Update OnGameActivityCreateHook

--- app/src/main/java/me/kyuubiran/akinatorhelper/HookEntry.kt
+++ app/src/main/java/me/kyuubiran/akinatorhelper/HookEntry.kt
@@ -3,6 +3,7 @@ package me.kyuubiran.akinatorhelper
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import me.kyuubiran.akinatorhelper.hooks.BaseHook
 import me.kyuubiran.akinatorhelper.hooks.ConfigHook
 import me.kyuubiran.akinatorhelper.hooks.OnGameActivityCreateHook
 
@@ -16,14 +17,11 @@ class HookEntry : IXposedHookLoadPackage {
     }
 
     private val init: Unit by lazy {
-        initHooks()
+        initHooks(OnGameActivityCreateHook, ConfigHook)
     }
 
-    private fun initHooks() {
-        listOf(
-            OnGameActivityCreateHook,
-            ConfigHook
-        ).forEach {
+    private fun initHooks(vararg hooks: BaseHook) {
+        hooks.forEach {
             if (it.isInit) return
             it.init()
             it.isInit = true
--- app/src/main/java/me/kyuubiran/akinatorhelper/hooks/OnGameActivityCreateHook.kt
+++ app/src/main/java/me/kyuubiran/akinatorhelper/hooks/OnGameActivityCreateHook.kt
@@ -2,7 +2,7 @@ package me.kyuubiran.akinatorhelper.hooks
 
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.utils.Log
-import com.github.kyuubiran.ezxhelper.utils.findMethodByCondition
+import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import de.robv.android.xposed.XC_MethodHook
 
@@ -10,8 +10,8 @@ object OnGameActivityCreateHook : BaseHook() {
     private var hook: XC_MethodHook.Unhook? = null
 
     override fun init() {
-        hook = findMethodByCondition("com.digidust.elokence.akinator.activities.AkActivity") {
-            it.name == "onCreate"
+        hook = findMethod("com.digidust.elokence.akinator.activities.AkActivity") {
+            name == "onCreate"
         }.hookAfter {
             hook?.unhook()
             EzXHelperInit.initAppContext()
